069129648612cabd10ee504a7b2a9fc1e1bf50ab
kotlet1c <piotr@gebczyk.pl>
2024-09-30 11:48:33 +0200

    Added script via GitHub upload.

---
+++ X4hQ.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+bookid=$1
+
+getname() {
+  curl -s "https://flipbook.apps.gwo.pl/display/$bookid" |
+  grep -oP '(?<=<title>).*?(?=</title>)' | tr -d ' '
+}
+
+
+name=$(getname)
+mkdir $name
+
+downloadbook() {
+  i=0
+  retVal=0
+  while [ $retVal -eq 0 ]; do
+    ((i++))
+    wget "https://flipbook.apps.gwo.pl/book/getImage/bookId:$bookid/pageNo:$i.jpg" -P "$name/"
+    retVal=$? 
+  done
+}
+
+merge () {
+  magick $(ls -v $name/*.jpg) $name.pdf
+}
+
+clean () {
+  rm -rf $name/
+}
+
+downloadbook
+merge
+clean
+#todo
+#add searching throught titles
+#merging into pdf
+ 
